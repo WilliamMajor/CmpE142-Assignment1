44a5cc235c255693b056a15a76c78e5b2b1c134a
WilliamMajor <epiphone1995@gmail.com>
2019-03-06 16:31:36 -0800

    Rough Setup

---
+++ Test/src/SuperShell.c
@@ -0,0 +1,26 @@
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int
+main(int argc, char **argv)
+{
+    char *line = NULL;
+    size_t linesize = 0;
+    size_t linelen;
+    while ((linelen = getline(&line, &linesize, stdin)) != -1) 
+    {
+        if (strncmp("exit", line, 4) == 0) 
+        {
+            // user wants to exit
+            exit(0);
+        }
+        fwrite(line, linelen, 1, stdout);
+        // fork
+        // if child, exec
+        // if parent, wait
+        }
+        free(line);
+        if (ferror(stdin))
+            err(1, "getline");        
+}
